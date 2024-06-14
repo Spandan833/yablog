9463437d170c2c9f57a50c7b82062ca35341cc1c
spandansenchoudhury <Spandan833@gmail.com>
2024-06-14 22:52:07 +0530

    Finished home page, articles, article, tags functions

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Recipe for Disaster';
 export const SITE_DESCRIPTION = 'Random Gibberish'
-export const HOMEPAGE_ARTICLE_LIMIT = 7
+export const HOMEPAGE_ARTICLE_LIMIT = 6
 export const ARTICLES_PER_PAGE = 6
