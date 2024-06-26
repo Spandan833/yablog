9284b51c8cdb1a35d77fc829159a5d8b5d066d43
spandansenchoudhury <Spandan833@gmail.com>
2024-06-26 22:05:32 +0530

    site title change

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const SITE_TITLE = 'Recipe for Disaster';
+export const SITE_TITLE = 'YaBlog';
 export const SITE_DESCRIPTION = 'Random Gibberish'
 export const HOMEPAGE_ARTICLE_LIMIT = 6
 export const ARTICLES_PER_PAGE = 6
-export const TESTIMONIAL = "Cuddy told me to write something nice about this page"
+export const TESTIMONIAL = "The number one place to catch up on latest tech advancements"
