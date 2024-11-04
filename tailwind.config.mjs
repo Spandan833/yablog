6e2df5a03c6ebb9b7feaba9a9604425bc87613ae
Sunfyre833 <Spandanssc@gmail.com>
2024-11-05 00:44:12 +0530

    Design changes

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "grid-flow": "grid-flow 20s linear infinite",
+      },
+      keyframes: {
+        "grid-flow": {
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(100%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
